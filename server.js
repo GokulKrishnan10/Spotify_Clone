23b8f4a058e49fe0a0e216dd0443042861f2dfa5
GokulKrishnan10 <gokulkrish.elangovan10@gmail.com>
2023-01-28 23:14:42 +0530

    Removed old code and changed the functionalities inside the end points based on the user schema

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const connection = require("./connect");
+const User = require("./user");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.listen(3000, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
@@ -42,55 +41,36 @@ app.get("/:id", (req, res) => {
 
 app.post("/submitted", (req, res) => {
   var forms = req.body;
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("FORM");
-    var myobj = {
-      mail: forms.mail,
-      password: forms.password,
-      birthday: forms.birthday,
-    };
-    dbo
-      .collection("customers")
-      .find({ mail: forms.mail })
-      .toArray((error, result) => {
-        if (result.length !== 0) {
-          res.write(
-            `<h1>MAIL ALREADY TAKEN <a href="register">REGISTER</a></h1>`
-          );
-          res.end();
-          return;
-        } else {
-          dbo.collection("customers").insertOne(myobj, function (err, res1) {
-            if (err) throw err;
-            res.write(`<h1>Successfully Created Account</h1>
-                       <h2>Welcome, ${forms.mail}</h2>`);
-            res.end();
-            db.close();
-          });
-        }
-      });
+  console.log(forms);
+  // MongoClient.connect(url, function (err, db) {
+  //   if (err) throw err;
+  //   var dbo = db.db("FORM");
+  const newUser = new User({
+    email: forms.mail,
+    password: forms.password,
+    dob: forms.birthday,
+    gender: forms.gender,
+  });
+  newUser.save(function (error) {
+    console.log("error occured ", error);
+    if (error === null) res.send("<h1>Successfully Created</h1>");
+    else res.send("<h1>User Exists</h1>");
   });
 });
 
 app.post("/loggedin", (req, res) => {
   var forms = req.body;
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("FORM");
-    dbo
-      .collection("customers")
-      .find({ mail: forms.mail, password: forms.password })
-      .toArray((err, result) => {
-        console.log(!result);
-        if (result.length === 0) {
-          res.send("REGISTER FIRST");
-          return;
-        }
-        res.send(`Successfully LOGGED IN
-        <br>
-        <h1><a href="spotify">HOME PAGE</a></h1>`);
-        console.log(result);
-      });
-  });
+  const newUser = {
+    email: forms.mail,
+    password: forms.password,
+  };
+  User.find(newUser)
+    .then((data) => {
+      console.log(data);
+      if (data.length === 0) res.send("<h1>User Does not exist</h1>");
+      else res.send("<h1>Successfully Logged in</h1>");
+    })
+    .catch((error) => {
+      res.send("User doesn't exist");
+    });
 });
